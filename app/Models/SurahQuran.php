adba1e8df152c76676cdf07c960df1e3f4b8dd5d
eviasasi <eviasasikhusnia@gmail.com>
2022-06-15 21:02:37 +0700

    first commit

---
+++ app/Models/SurahQuran.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SurahQuran extends Model
+{
+    use HasFactory;
+    protected $table = 'surah_quran';
+    protected $guarded = [];
+}
---
+++ database/migrations/2022_06_15_130028_create_profile_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('nama_siswa');
+            $table->string('jenis_kelamin');
+            $table->date('tgl_lahir');
+            $table->string('tempat_lahir');
+            $table->string('alamat');
+            $table->string('nama_ayah');
+            $table->string('nama_ibu');
+            $table->string('pekerjaan_ayah');
+            $table->string('pekerjaan_ibu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+};
